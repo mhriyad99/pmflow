b20c3fe25e84fddb6c1594cce4fc0e84805b1afd
mhriyad99 <mhriyad98@gmail.com>
2024-10-20 12:34:38 +0600

    oct_20_sun: key->pid, first_stable

--- pm/main.py
+++ pm/main.py
@@ -9,19 +9,12 @@ import psutil
 import subprocess
 from rich.console import Console
 from rich.table import Table
+from pm.utils import StateManager
 
 app = typer.Typer()
 
-def load_state():
-    """Load processes state from a file."""
-    global processes
-    if os.path.exists(STATE_FILE):
-        with open(STATE_FILE, "r") as file:
-            processes = json.load(file)
-
-processes = {}
-STATE_FILE = os.getcwd() + '/processes_state.json'
-load_state()
+STATE_FILE = os.path.join(os.path.dirname(__file__), 'processes_state.json')
+state = StateManager(STATE_FILE)
 
 @app.command()
 def greet(name: str):
@@ -37,69 +30,34 @@ def greet(name: str):
 @app.command()
 def create(command: str, name: str = None) -> int:
     """Create a new subprocess and optionally assign a name."""
-    proc = subprocess.Popen(command, shell=True)
+    proc = subprocess.Popen(command, shell=False)
     pid = proc.pid
-    processes[pid] = {
+    data = {
         "command": command,
         "name": name,
-        "status": "running"
     }
-    save_state()
+    state.add_process(pid, data)
     typer.echo(f"{pid}")
 
-    # Return the PID for external usage
-    return pid
-
-@app.command()
-def state_load():
-    # load_state()
-    typer.echo(json.dumps(processes))
-
-@app.command()
-def state_location():
-    typer.echo(STATE_FILE)
 
 @app.command()
 def pause(pid: int):
     """Pause a subprocess and all its children by PID."""
     pid_str = str(pid)
-    if pid_str in processes:
+    if pid_str in state.processes:
         try:
             process = psutil.Process(int(pid))
             all_processes = [process] + process.children(recursive=True)
             for proc in all_processes:
                 proc.send_signal(signal.SIGSTOP)
             # Update process status to 'paused' and save state
-            processes[pid_str] = {
-                "command": processes[pid_str],
-                "status": "paused"
-            }
-            save_state()
+            state.update_process(pid, "status", "paused")
             typer.echo(f"Process {pid} and its child processes have been paused.")
         except psutil.NoSuchProcess:
             typer.echo("Process not found.")
     else:
         typer.echo("Process not managed by this tool.")
 
-
-@app.command()
-def kill(pid: int):
-    """Kill a subprocess by PID."""
-    if str(pid) in processes:
-        try:
-            process = psutil.Process(pid)
-            for child in process.children(recursive=True):
-                child.terminate()
-            process.terminate()
-            processes.pop(str(pid), None)
-            save_state()
-            typer.echo(f"Process {pid} killed.")
-        except psutil.NoSuchProcess:
-            processes.pop(str(pid), None)
-            save_state()
-            typer.echo("Process not found. Removed from the state file.")
-    else:
-        typer.echo("Process not managed by this tool.")
 
 @app.command()
 def ls():
@@ -110,10 +68,16 @@ def ls():
     table.add_column("Status", justify="right", style="green")
     table.add_column("Command", justify="right", style="yellow")
 
-    for pid, properties in processes.items():
-        typer.echo(f"How are you?")
-        process = psutil.Process(int(pid))
-        status = 'running' if process.is_running() else 'paused'
+    for pid, properties in state.processes.items():
+        try:
+            process = psutil.Process(int(pid))
+            if process.status() == psutil.STATUS_STOPPED:
+                status = 'paused'
+            else:
+                status = 'running'
+        except psutil.NoSuchProcess:
+            status = "doesn't exist"
+
         table.add_row(pid, properties["name"], status, properties["command"])
 
     console = Console()
@@ -122,37 +86,51 @@ def ls():
 @app.command()
 def recreate():
     """Recreate all managed subprocesses."""
-    load_state()
-    for pid, command in processes.items():
-        proc = subprocess.Popen(command, shell=True)
-        processes[proc.pid] = command
-        typer.echo(f"Process {proc.pid} recreated with command: {command}")
-    save_state()
+    for pid, data in state.processes.items():
+        proc = subprocess.Popen(data["command"], shell=True)
+        state.remove_process(pid)
+        state.add_process(proc.pid, data)
+        typer.echo(f"Process {proc.pid} recreated with command: {data['command']}")
+
 
 @app.command()
-def respawn():
+def respawn_all():
     """Respawn processes that are in the JSON file but not running."""
-    load_state()
-    new_processes = {}
-    for pid, command in processes.items():
-        if not psutil.pid_exists(int(pid)):
-            typer.echo(f"Process {pid} not running. Respawning...")
-            proc = subprocess.Popen(command, shell=False)
-            new_processes[proc.pid] = command
-            typer.echo(f"Process {proc.pid} respawned with command: {command}")
-        else:
-            new_processes[int(pid)] = command
-    processes.clear()
-    processes.update(new_processes)
-    save_state()
+
+    for pid, data in state.processes.items():
+        if psutil.pid_exists(int(pid)):
+            process = psutil.Process(int(pid))
+            if not process.is_running():
+                typer.echo(f"Process {pid} not running. Respawning...")
+                process.resume()
+                typer.echo(f"Process {pid} respawed.")
+
     typer.echo("Respawn complete.")
+
+
+@app.command()
+def kill(pid: int):
+    """Kill a subprocess by PID."""
+    if str(pid) in state.processes:
+        try:
+            process = psutil.Process(pid)
+            for child in process.children(recursive=True):
+                child.terminate()
+            process.terminate()
+            state.remove_process(pid)
+            typer.echo(f"Process {pid} killed.")
+        except psutil.NoSuchProcess:
+            state.remove_process(pid)
+            typer.echo("Process not found. Removed from the state file.")
+    else:
+        typer.echo("Process not managed by this tool.")
 
 
 @app.command()
 def kill_all():
     """Kill all managed processes and clear the state."""
-    load_state()
-    for pid in processes.keys():
+
+    for pid in state.processes.keys():
         try:
             process = psutil.Process(int(pid))
             for child in process.children(recursive=True):
@@ -164,14 +142,13 @@ def kill_all():
         except Exception as e:
             typer.echo(f"Error terminating process {pid}: {str(e)}")
 
-    processes.clear()
-    save_state()
+    state.remove_all_processes()
     typer.echo("All processes have been terminated and removed from the state.")
 
-def save_state():
-    """Save current processes state to a file."""
-    with open(STATE_FILE, "w") as file:
-        json.dump(processes, file)
+# def save_state():
+#     """Save current processes state to a file."""
+#     with open(STATE_FILE, "w") as file:
+#         json.dump(processes, file)
 
 
 def signal_handler(sig, frame):
